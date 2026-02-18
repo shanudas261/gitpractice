5e814aae511f9ed869d73ec00358312acb19d05c
shanudas261 <shanudas261@gmail.com>
2026-02-18 10:02:53 +0530

    getelementbyid

--- shargit.js
+++ shargit.js
@@ -1,1 +1,18 @@
-console.log('git started')
+// Write your code below:
+const heading = document.getElementById('main-heading')
+
+heading.textContent = 'Fruit World'
+heading.style.color = 'orange'
+
+const header = document.getElementById('header')
+
+header.style.backgroundColor = 'green'
+header.style.borderBottom = '3px solid orange'
+
+const basketHeading = document.getElementById('basket-heading')
+
+basketHeading.style.color='green'
+
+const thanks = document.getElementById('thanks')
+
+thanks.innerHTML='<p>Please visit us again</p>'
