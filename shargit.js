a3daedb3142048b7a87357ec8cf5497f40a6a7ab
shanudas261 <shanudas261@gmail.com>
2026-02-18 10:07:50 +0530

    getElementByClassName

--- shargit.js
+++ shargit.js
@@ -1,18 +1,6 @@
-// Write your code below:
-const heading = document.getElementById('main-heading')
+const fruits = document.getElementsByClassName('fruit')
+fruits[2].style.backgroundColor = 'yellow'
 
-heading.textContent = 'Fruit World'
-heading.style.color = 'orange'
-
-const header = document.getElementById('header')
-
-header.style.backgroundColor = 'green'
-header.style.borderBottom = '3px solid orange'
-
-const basketHeading = document.getElementById('basket-heading')
-
-basketHeading.style.color='green'
-
-const thanks = document.getElementById('thanks')
-
-thanks.innerHTML='<p>Please visit us again</p>'
+for (let i = 0; i < 4; i++){
+    fruits[i].style.fontWeight='bold'
+}
